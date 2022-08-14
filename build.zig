b7a78ecb1a4e99325dda61e9122f2eacccca5c3d
Lee Cannon <leecannon@leecannon.xyz>
2022-08-14 20:32:41 +0100

    support verbs with void and zero sized payload types

--- build.zig
+++ build.zig
@@ -33,10 +33,20 @@ pub fn build(b: *std.build.Builder) void {
     run_demo_verb_2.addArgs(&[_][]const u8{
         "reload", "-f",
     });
+    const run_demo_verb_3 = demo_verb_exe.run();
+    run_demo_verb_3.addArgs(&[_][]const u8{
+        "forward",
+    });
+    const run_demo_verb_4 = demo_verb_exe.run();
+    run_demo_verb_4.addArgs(&[_][]const u8{
+        "zero-sized",
+    });
 
     const test_step = b.step("test", "Runs the test suite.");
     test_step.dependOn(&test_runner.step);
     test_step.dependOn(&run_demo.step);
     test_step.dependOn(&run_demo_verb_1.step);
     test_step.dependOn(&run_demo_verb_2.step);
+    test_step.dependOn(&run_demo_verb_3.step);
+    test_step.dependOn(&run_demo_verb_4.step);
 }
