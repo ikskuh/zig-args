11d0f07c1fae8200fa10602a8b84969132d949a6
Felix Queißner <felix@ib-queissner.de>
2026-04-16 14:22:40 +0200

    Fixes demo.zig

--- demo.zig
+++ demo.zig
@@ -1,45 +1,43 @@
 const std = @import("std");
 const argsParser = @import("args");
 
-pub fn main() !u8 {
-    const argsAllocator = std.heap.page_allocator;
+const Options = struct {
+    // This declares long options for double hyphen
+    output: ?[]const u8 = null,
+    @"with-offset": bool = false,
+    @"with-hexdump": bool = false,
+    @"intermix-source": bool = false,
+    numberOfBytes: ?i32 = null,
+    signed_number: ?i64 = null,
+    unsigned_number: ?u64 = null,
+    mode: enum { default, special, slow, fast } = .default,
+    help: bool = false,
 
-    const Options = struct {
-        // This declares long options for double hyphen
-        output: ?[]const u8 = null,
-        @"with-offset": bool = false,
-        @"with-hexdump": bool = false,
-        @"intermix-source": bool = false,
-        numberOfBytes: ?i32 = null,
-        signed_number: ?i64 = null,
-        unsigned_number: ?u64 = null,
-        mode: enum { default, special, slow, fast } = .default,
-        help: bool = false,
-
-        // This declares short-hand options for single hyphen
-        pub const shorthands = .{
-            .S = "intermix-source",
-            .b = "with-hexdump",
-            .O = "with-offset",
-            .o = "output",
-        };
-
-        pub const meta = .{
-            .option_docs = .{
-                .output = "output help",
-                .@"with-offset" = "with-offset help",
-                .@"with-hexdump" = "with-hexdump help",
-                .@"intermix-source" = "intermix-source",
-                .numberOfBytes = "numberOfBytes help",
-                .signed_number = "signed_number help",
-                .unsigned_number = "unsigned_number help",
-                .mode = "mode help",
-                .help = "help help",
-            },
-        };
+    // This declares short-hand options for single hyphen
+    pub const shorthands = .{
+        .S = "intermix-source",
+        .b = "with-hexdump",
+        .O = "with-offset",
+        .o = "output",
     };
 
-    const options = argsParser.parseForCurrentProcess(Options, argsAllocator, .print) catch return 1;
+    pub const meta = .{
+        .option_docs = .{
+            .output = "output help",
+            .@"with-offset" = "with-offset help",
+            .@"with-hexdump" = "with-hexdump help",
+            .@"intermix-source" = "intermix-source",
+            .numberOfBytes = "numberOfBytes help",
+            .signed_number = "signed_number help",
+            .unsigned_number = "unsigned_number help",
+            .mode = "mode help",
+            .help = "help help",
+        },
+    };
+};
+
+pub fn main(init: std.process.Init) !u8 {
+    const options = argsParser.parseForCurrentProcess(Options, init.arena, init.minimal.args, .print) catch return 1;
     defer options.deinit();
 
     std.debug.print("executable name: {?s}\n", .{options.executable_name});
